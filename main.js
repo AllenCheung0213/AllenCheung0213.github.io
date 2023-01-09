b791353973b384bb92ff9f3003543b01e56f3ce6
AllenCheung0213 <alcheung0213@gmail.com>
2023-01-08 19:29:30 -0500

    change back
    Change back

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-<script src="script.js" type="text/javascript"></script>
 import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-href="/assets/main.css"
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
